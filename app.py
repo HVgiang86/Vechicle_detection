4f433a648be5b7bd570afbefe2b9c64ef3aaf4a4
Giang Hoang Van <hvgiang86@gmail.com>
2024-01-09 22:02:18 +0700

    add run directly from app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import base64
 import io
 import json
-import threading
 
 import socketio
 from PIL import Image
@@ -9,6 +8,10 @@ from PIL import Image
 from track import *
 
 sio = socketio.Client()
+sio.connect('http://127.0.0.1:5000')
+
+# socket_server = SocketIO(app)
+fileName = 'Cars Moving On Road Stock Footage - Free Download.mp4'
 
 
 # define event
@@ -20,13 +23,6 @@ def connect():
 @sio.event
 def disconnect():
     print("Disconnected from the server")
-
-
-# socket_server = SocketIO(app)
-fileName1 = 'Cars Moving On Road Stock Footage - Free Download.mp4'
-fileName2 = 'Cars Moving On Road Stock Footage - Free Download.mp4'
-fileName3 = 'Cars Moving On Road Stock Footage - Free Download.mp4'
-fileName4 = 'Cars Moving On Road Stock Footage - Free Download.mp4'
 
 
 def handleImg(img):
@@ -46,15 +42,33 @@ def handleImg(img):
     image_bytes = image_stream.read()
 
     base64Img = base64.b64encode(image_bytes).decode('utf-8')
+    # print('base64: ', base64Img)
 
     return base64Img
 
 
-def thread_function(fileName, event):
-    start_demo_detection(fileName, event, detection_callback)
+def end_of_stream_base64():
+    image_path = 'videos/endofstream.png'
+    # Read the image file in binary mode
+    with open(image_path, 'rb') as image_file:
+        binary_data = image_file.read()
+    # Encode the binary data to base64
+    base64_encoded_data = base64.b64encode(binary_data)
+    # Convert to a string for easier use
+    base64_string = base64_encoded_data.decode('utf-8')
+    return base64_string
+
+
+def end_of_stream_data():
+    # Emit to socket client from here
+
+    data = {"base64Img": end_of_stream_base64(), "current_vehicle_per_fps": 0, "data_car": 0, "data_bus": 0,
+            "data_truck": 0, "data_motor": len(data_motor), "fps": 0}
 
 
 def detection_callback(event, result_image, current_vehicle, data_car, data_bus, data_truck, data_motor, fps):
+    # print('event: ', event)
+    # print('buffer: ', result_image)
     base64Img = handleImg(result_image)
     # print("Callback")
     # print('base64Img: ', base64Img)
@@ -67,6 +81,9 @@ def detection_callback(event, result_image, current_vehicle, data_car, data_bus,
     # Emit to socket client from here
     data = {"base64Img": base64Img, "current_vehicle_per_fps": len(current_vehicle), "data_car": len(data_car),
             "data_bus": len(data_bus), "data_truck": len(data_truck), "data_motor": len(data_motor), "fps": fps}
+
+    print(event + '\tcurrent: ' + str(len(current_vehicle)))
+    print('car: ', len(data_car))
 
     json_string = json.dumps(data)
     sio.emit(event, json_string)
@@ -93,20 +110,5 @@ def start_demo_detection(fileName, event, result_callback):
         detect(opt, line, assigned_class_id, event, result_callback)
 
 
-sio.connect('http://127.0.0.1:5000')
-
 if __name__ == '__main__':
-    if sio.connected:
-        thread1 = threading.Thread(target=thread_function, args=(f"{fileName1}", "r1_ai"))
-        thread1.start()
-
-        thread2 = threading.Thread(target=thread_function, args=(f"{fileName2}", "r2_ai"))
-        thread2.start()
-
-        thread3 = threading.Thread(target=thread_function, args=(f"{fileName3}", "r3_ai"))
-        thread3.start()
-
-        thread4 = threading.Thread(target=thread_function, args=(f"{fileName4}", "r4_ai"))
-        thread4.start()
-    else:
-        print("Not connected to socket server")
+    start_demo_detection(fileName, 'r1_ai', detection_callback)
